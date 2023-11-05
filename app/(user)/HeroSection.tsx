667c8e5c23af8c7bd88d28a503c225a332328610
Vahid <vahid.tahmasebi95@gmail.com>
2023-11-05 18:14:07 +0330

    Add travel search to section hero

--- app/(user)/HeroSection.tsx
+++ app/(user)/HeroSection.tsx
@@ -38,6 +38,48 @@ const HeroSection = () => {
           />
         </div>
       </div>
+      <div className="max-w-2xl h-16 flex justify-around items-center px-5 rounded-full shadow-2xl shadow-c-primary-600/70">
+        <button className="flex gap-4">
+          <span>Where</span>
+          <Image
+            src="/assets/icons/place-icon.svg"
+            alt=""
+            width={20}
+            height={20}
+            className="object-contain"
+          />
+        </button>
+        <div className="w-1 h-10 rounded-xl bg-c-primary-600/30" />
+        <button className="flex gap-4">
+          <span>When</span>
+          <Image
+            src="/assets/icons/calendar-search-icon.svg"
+            alt=""
+            width={20}
+            height={20}
+            className="object-contain"
+          />
+        </button>
+        <div className="w-1 h-10 rounded-xl bg-c-primary-600/30" />
+        <button className="flex gap-4">
+          <span>Travel Type</span>
+          <Image
+            src="/assets/icons/bus-icon.svg"
+            alt=""
+            width={20}
+            height={20}
+            className="object-contain"
+          />
+        </button>
+        <Button
+          width="w-36"
+          height="h-12"
+          padding="px-5"
+          color="text-[#f0fdf4]"
+          background="bg-c-primary-500">
+          Search
+        </Button>
+      </div>
     </>
   );
 };
