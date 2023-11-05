2a57dcec64a0f7081fdecbec0ef11c5c276c84f6
Vahid <vahid.tahmasebi95@gmail.com>
2023-11-05 17:54:42 +0330

    Add button component

---
+++ components/buttons/Button.tsx
@@ -0,0 +1,43 @@
+import classnames from "classnames";
+
+import { HtmlHTMLAttributes, ReactElement } from "react";
+
+interface Props extends HtmlHTMLAttributes<any> {
+  children: React.ReactNode;
+  width: string;
+  height: string;
+  padding: string;
+  borderColor?: string;
+  color?: string;
+  background?: string;
+}
+
+const Button = ({
+  children,
+  width,
+  height,
+  padding,
+  borderColor,
+  color,
+  background,
+}: Props): ReactElement => {
+  const classStr = classnames(
+    "border rounded-3xl text-base font-medium",
+    width,
+    height,
+    padding,
+    borderColor,
+    color,
+    background
+  );
+
+  return (
+    <button
+      //   onClick={onClick}
+      className={classStr}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
