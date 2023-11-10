657af29a0b60c183022fe4e05aed14b0b64be451
Vahid <vahid.tahmasebi95@gmail.com>
2023-11-10 13:57:26 +0330

    Add lazy load for hotel cart

---
+++ common/LazyLoads/CartLazyLoad.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+const CartLazyLoad = () => {
+  return (
+    <>
+      <div className="absolute top-4 right-7">
+        <Image
+          src="/assets/icons/star-lazy-icon.svg"
+          alt=""
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-6 h-6 animate-pulse"
+        />
+      </div>
+      <div className="cart__lazy--class w-1/6 bottom-10 left-7" />
+      <div className="cart__lazy--class w-1/2 bottom-4 left-7" />
+    </>
+  );
+};
+
+export default CartLazyLoad;
