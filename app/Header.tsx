888170187e96fd5e66824755b96d6862bdf0d8ae
Vahid <vahid.tahmasebi95@gmail.com>
2023-11-14 18:51:32 +0330

    Add flag icon in header

--- app/Header.tsx
+++ app/Header.tsx
@@ -20,7 +20,7 @@ function Header() {
   ];
 
   return (
-    <header className="sticky top-2 z-10 flex items-center justify-between px-5 rounded-3xl bg-c-surface-100/70 backdrop-blur-md shadow-xl shadow-c-surface-600/10 transition-all duration-200">
+    <header className="sticky top-2 z-10 flex items-center justify-between gap-x-2 px-5 rounded-3xl bg-c-surface-100/70 backdrop-blur-md shadow-xl shadow-c-surface-600/10 transition-all duration-200">
       <Link className="block py-2" href="/">
         <Image
           priority={true}
@@ -33,7 +33,7 @@ function Header() {
         />
       </Link>
       <nav>
-        <ul className="h-16 flex items-center sm:space-x-8 md:space-x-20 py-2 font-bold">
+        <ul className="h-16 flex items-center sm:space-x-5 md:space-x-16 py-2 font-bold">
           {routeMenu.map((route, index) => {
             const isActive =
               (pathname.includes(route.href) && route.href.length > 1) ||
@@ -50,11 +50,11 @@ function Header() {
               </li>
             );
           })}
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2 sm:gap-4">
             <Button
-              width="w-full"
+              width="w-fit sm:w-full"
               height="h-10"
-              padding="px-5"
+              padding="px-2"
               color="text-[#FFE5D4]"
               background="bg-c-orange-500">
               <span className="hidden sm:block">Login | Signup</span>
@@ -65,9 +65,25 @@ function Header() {
                 width="0"
                 height="0"
                 sizes="100vw"
-                className="icon--class sm:ml-3"
+                className="icon--class ml-0 sm:ml-2"
               />
             </Button>
+
+            <div className="w-9 h-9 relative">
+              <Image
+                src={`/assets/icons/flag-${
+                  theme === "dark" ? "dark" : "light"
+                }-icon.svg`}
+                alt=""
+                width="0"
+                height="0"
+                sizes="100vw"
+                className="w-9 h-9"
+              />
+              <span className="absolute -bottom-2 sm:-bottom-1 left-3 sm:left-2.5 text-sm text-c-primary-950">
+                EN
+              </span>
+            </div>
             <ToggleTheme />
           </div>
         </ul>
