dc8d8b5f40885ac6054b348b955de090cdb8f669
Vahid <vahid.tahmasebi95@gmail.com>
2023-11-22 16:08:27 +0330

    Fix bug in blog

--- app/[lang]/(user)/blog/[id]/page.tsx
+++ app/[lang]/(user)/blog/[id]/page.tsx
@@ -14,29 +14,28 @@ interface IBlogIdProps {
 const BlogId: FC<IBlogIdProps> = ({ params: { lang, id } }) => {
   const { isLoading, data } = useGetHotelsById(id);
 
-  console.log(data?.picture_url?.url);
-  console.log(data?.name);
-
   return (
     <div className="w-fit relative">
-      <Image
-        src={data?.picture_url?.url}
-        alt={data?.name}
-        placeholder="blur"
-        blurDataURL={data?.picture_url?.url}
-        width={0}
-        height={0}
-        sizes="100vw"
-        className={`w-full h-96 block rounded-3xl object-cover`}
-      />
-      <div className="cart--class bottom-4 start-7 text-start">
-        <h5
-          className={`cart__text--class w-4/6 rounded-b-xl  ${
-            lang === "en" ? "rounded-tr-xl" : "rounded-tl-xl"
-          }`}>
-          {data?.description}
-        </h5>
-      </div>
+      {data && (
+        <>
+          <Image
+            src={data?.picture_url?.url}
+            alt={data?.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className={`w-full h-96 block rounded-3xl object-cover`}
+          />
+          <div className="cart--class bottom-4 start-7 text-start">
+            <h5
+              className={`cart__text--class w-4/6 rounded-b-xl  ${
+                lang === "en" ? "rounded-tr-xl" : "rounded-tl-xl"
+              }`}>
+              {data?.description}
+            </h5>
+          </div>
+        </>
+      )}
     </div>
   );
 };
