d3c13bdcc35850bb0908b8a3000f06158ee0843f
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-17 11:12:05 +0330

    Add debounce to search form

--- app/[lang]/(user)/components/SearchForm.tsx
+++ app/[lang]/(user)/components/SearchForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { FC } from "react";
-import { usePathname, useRouter } from "next/navigation";
+import React, { FC, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import Image from "next/image";
 import { useForm } from "react-hook-form";
 
@@ -12,6 +12,9 @@ import searchIcon from "@/public/assets/icons/search-icon.svg";
 import { Locale } from "@/i18n.config";
 
 import { dictionary } from "@/constants/dictionaries";
+
+import useDebounce from "@/hooks/useDebounce";
+import { useGetHotels } from "@/hooks/useHotels";
 
 import Button from "@/common/buttons/Button";
 import TextField from "@/common/Inputs/TextField";
@@ -36,6 +39,11 @@ const SearchForm: FC<SearchFormProps> = ({ lang }) => {
   const router = useRouter();
   const pathname = usePathname();
 
+  const [search, setSearch] = useState("");
+  const [filteredTitle, setFilteredTitle] = useState([]);
+
+  const { isLoading, data } = useGetHotels();
+
   const initialOptions: Option[] = [
     { value: dictionary[lang]?.trainWord },
     { value: dictionary[lang]?.airplaneWord },
@@ -46,6 +54,9 @@ const SearchForm: FC<SearchFormProps> = ({ lang }) => {
     register,
     handleSubmit,
     control,
+    setValue,
+    getValues,
+    watch,
     formState: { errors },
   } = useForm<FormData>({
     defaultValues: {
@@ -55,13 +66,23 @@ const SearchForm: FC<SearchFormProps> = ({ lang }) => {
     },
   });
 
+  useDebounce(
+    () => {
+      setFilteredTitle(
+        data.filter((item) =>
+          item.name.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    },
+    [data, search],
+    1200
+  );
+
   const onFormSubmit = async (values: {
     whereValue: string;
     whenValue: any;
     travelTypeValue: string;
   }) => {
-    console.log(values);
-
     if (pathname === "/search") {
       router.back();
     } else {
@@ -72,17 +93,30 @@ const SearchForm: FC<SearchFormProps> = ({ lang }) => {
   return (
     <form
       onSubmit={handleSubmit(onFormSubmit)}
-      className="sm:max-w-3xl sm:h-16 flex flex-col sm:flex-row justify-around items-start sm:items-center gap-y-8 gap-x-4 p-4 sm:px-3 rounded-3xl sm:rounded-full bg-c-surface-200/50 shadow-xl shadow-c-primary-500/30">
-      <TextField
-        register={register}
-        type="text"
-        name="whereValue"
-        placeholder={dictionary[lang]?.whereWord}
-        inputMode=""
-        min={2}
-        maxLength={8}
-        icon={placeIcon}
-      />
+      className="sm:max-w-3xl sm:h-16 flex flex-col sm:flex-row justify-around items-start sm:items-center gap-y-8 gap-x-4 p-4 sm:px-3 rounded-3xl sm:rounded-full bg-c-surface-200/50 shadow-xl shadow-c-primary-500/10">
+      <div className="w-4/6 h-full relative flex flex-col">
+        <TextField
+          register={register}
+          type="text"
+          name="whereValue"
+          placeholder={dictionary[lang]?.whereWord}
+          inputMode=""
+          min={2}
+          maxLength={8}
+          icon={placeIcon}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="w-full h-fit max-h-56 absolute top-10 z-50 px-3 divide-y-2 divide-c-surface-300 rounded-xl bg-c-surface-100 overflow-hidden overflow-y-auto">
+          {search &&
+            filteredTitle.map((item) => (
+              <div
+                key={item.id}
+                onClick={() => setValue("whereValue", item.name)}>
+                <p className="py-2 text-xs text-c-surface-950">{item.name}</p>
+              </div>
+            ))}
+        </div>
+      </div>
 
       <DatePickerComponent
         control={control}
